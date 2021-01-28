ecf5284af27811cb14e4c7b5f8d5802c8a030ec8
lixin <2690688423@qq.com>
2021-01-28 22:17:17 +0800

    upgrade

---
+++ Fundamentals/BinarySearch.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    // precondition: array a[] is sorted
+    public static int rank(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] whitelist = In.readInts(args[0]);
+
+        Arrays.sort(whitelist);
+
+        // read key; print if not in whitelist
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (rank(key, whitelist) == -1)
+                StdOut.println(key);
+        }
+    }
+}
