ad66b0e121772ec4e54353b2b05a192b3553bf7a
lixin <2690688423@qq.com>
2021-02-05 13:36:02 +0800

    upgrade

--- InputTest.java
+++ InputTest.java
@@ -5,7 +5,13 @@ public class InputTest{
         Scanner in = new Scanner(System.in);
         //get first name
         System.out.println("What is your name?");
-        
+        String name = in.nextLine();
 
+        //get second input
+        System.out.println("How old are you?");
+        int age = in.nextInt();
+
+        //display output on console
+        System.out.println("Hello,"+name+".Next year,you'll be "+ (age+1));
     }
 }
