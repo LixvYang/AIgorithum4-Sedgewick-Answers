fc0b17c6d99c56c5d8a6a522b2e6e483302241dd
lixin <2690688423@qq.com>
2021-02-05 13:31:31 +0800

    upgrade

---
+++ InputTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class InputTest{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        //get first name
+        System.out.println("What is your name?");
+        
+
+    }
+}
